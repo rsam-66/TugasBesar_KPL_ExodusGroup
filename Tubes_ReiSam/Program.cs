6453372a310c34ff4c8e6132fb243dd3b0ebfcee
rsam-66 <samuelriyanto57@gmail.com>
2024-05-14 03:21:15 +0700

    Membuat Konstruksi Automata untuk Halaman Produk dan Keranjang

--- Tubes_ReiSam/Program.cs
+++ Tubes_ReiSam/Program.cs
@@ -1,1 +1,103 @@
-﻿
+﻿using System;
+
+namespace Program
+{
+    class Program
+    {
+        enum state { PRODUK, KERANJANG, BAYAR, KELUAR };
+        static void Main(string[] args)
+        {
+            state posisi = state.PRODUK;
+            string[] screenName = { "PRODUK", "KERANJANG", "BAYAR", "KELUAR" };
+            while (posisi != state.KELUAR)
+            {
+                Console.WriteLine("Halaman " + screenName[(int)posisi]);
+
+                switch (posisi)
+                {
+                    case state.PRODUK:
+
+                        Console.WriteLine("");
+                        Console.WriteLine("1. Tambahkan Produk");
+                        Console.WriteLine("2. Lihat Keranjang");
+                        Console.WriteLine("0. Keluar");
+                        Console.WriteLine("");
+                        Console.WriteLine("Pilihan :");
+                        string aksi = Console.ReadLine();
+
+                        if (aksi == "1")
+                        {
+                            Console.WriteLine("Masukkan Nama Produk yang Ingin Ditambahkan");
+                            string nama_produk = Console.ReadLine();
+                            // kondisi barang apakah ada atau tidak
+                            // kondisi dibawah jika produk tersebut ada
+                            Console.WriteLine("Produk Ditambahkan!");
+                            Console.WriteLine("");
+                            posisi = state.PRODUK;
+                        }
+                        else if (aksi == "2")
+                        {
+                            Console.WriteLine("");
+                            posisi = state.KERANJANG;
+                        }
+                        else if (aksi == "0")
+                        {
+                            posisi = state.KELUAR;
+                        }
+                        else
+                        {
+                            Console.WriteLine("Pilihan Tidak Valid!");
+                            Console.WriteLine("");
+                            posisi = state.PRODUK;
+                        }
+                        break;
+
+                    case state.KERANJANG:
+                        Console.WriteLine("Keranjang Belanja Anda");
+                        // ditampilkan isi keranjang
+                        Console.WriteLine("");
+                        Console.WriteLine("3. Hapus Produk");
+                        Console.WriteLine("4. Bayar");
+                        Console.WriteLine("5. Kembali Ke Halaman Produk");
+                        Console.WriteLine("Pilihan :");
+
+                        string aksi2 = Console.ReadLine();
+
+                        if (aksi2 == "3")
+                        {
+                            Console.WriteLine("");
+                            Console.WriteLine("Masukkan Nama Produk yang Ingin Dihapus");
+                            string nama_produk = Console.ReadLine();
+                            // kondisi cek apakah produk ada atau tidak
+                            // kondisi produk ada
+                            Console.WriteLine("Produk Berhasil Dihapus!");
+                            Console.WriteLine("");
+                            posisi = state.KERANJANG;
+                        }
+                        else if (aksi2 == "4")
+                        {
+                            Console.WriteLine("");
+                            Console.WriteLine("Pilih pembayaran :");
+                            // kondisi memilih pembayaran
+                            // kondisi pembayaran berhasil
+                            Console.WriteLine("");
+                            Console.WriteLine("Terimakasih Telah Berbelanja!");
+                            Console.WriteLine("");
+                            posisi = state.KELUAR;
+                        }
+                        else if (aksi2 == "5") {
+                            Console.WriteLine("");
+                            posisi = state.PRODUK;
+                        } else {
+                            Console.WriteLine("Pilihan Tidak Valid!");
+                            Console.WriteLine("");
+                            posisi = state.KERANJANG;
+                        }
+                        break;
+                }
+
+            }
+            Console.WriteLine("Terimakasih Telah Menggunakan Prorgam Kami!");
+        }
+    }
+}
--- Tubes_ReiSam/obj/Release/net8.0/Tubes_ReiSam.AssemblyInfo.cs
+++ Tubes_ReiSam/obj/Release/net8.0/Tubes_ReiSam.AssemblyInfo.cs
@@ -12,7 +12,7 @@ using System;
 using System.Reflection;
 
 [assembly: System.Reflection.AssemblyCompanyAttribute("Tubes_ReiSam")]
-[assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
+[assembly: System.Reflection.AssemblyConfigurationAttribute("Release")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
 [assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+fb29b9b2924471d757005c856c976f4d048a6404")]
 [assembly: System.Reflection.AssemblyProductAttribute("Tubes_ReiSam")]
