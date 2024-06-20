009513faad31ec0da3296f71a2b1c584a52182b6
brdwja <103436437+brdwja@users.noreply.github.com>
2024-06-20 10:25:42 +0700

    Add files via upload

---
+++ TableDriven/ListViewHelper.cs
@@ -0,0 +1,21 @@
+﻿namespace SeedMeUtilities
+{
+    public static class ListViewHelper
+    {
+        public static void AddItem<T>(ListView listView, T item)
+        {
+            ListViewItem listViewItem = new ListViewItem(item.ToString());
+            listView.Items.Add(listViewItem);
+        }
+
+        public static void AddItemWithSubItems<T>(ListView listView, T item, params string[] subItems)
+        {
+            ListViewItem listViewItem = new ListViewItem(item.ToString());
+            foreach (var subItem in subItems)
+            {
+                listViewItem.SubItems.Add(subItem);
+            }
+            listView.Items.Add(listViewItem);
+        }
+    }
+}
